8040dc325ef63fb2176739845f78a7a87f2563c3
kokol16 <kokol1616@gmail.com>
2022-03-18 13:22:00 +0200

    added file names in graphs

--- iperf3_logs_processor.py
+++ iperf3_logs_processor.py
@@ -3,13 +3,24 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+def get_relative_path(absolute_path):
+    filename=""
+    for c in reversed(absolute_path):
+        if c=="/":
+            return filename[::-1]
+        else:
+            filename=filename+ c
+
+     
+
+
 def process_json_files():
     list_of_files=parser.parse_folder_with_json_files()
     #lists contain a dictionary for each json file
     list_of_stats_tcp=[]
     list_of_stats_udp=[]
     for file in list_of_files:
-        print(file)
         f = open(file)
         data = json.load(f)
         if data["start"]["test_start"]["protocol"]=="TCP":
@@ -21,6 +32,12 @@ def process_json_files():
                     if key not in dict_tcp: 
                         dict_tcp[key]=[]
                     dict_tcp[key].append(value)
+                if "filename" not in dict_tcp:
+                    dict_tcp["filename"]=[]
+                tmp_file_name= file
+                tmp_file_name= str(tmp_file_name)
+                dict_tcp["filename"].append(get_relative_path(str(file)))
+
             list_of_stats_tcp.append(dict_tcp)
 
         else:
@@ -32,6 +49,9 @@ def process_json_files():
                     if key not in dict_udp: 
                         dict_udp[key]=[]
                     dict_udp[key].append(value)
+                if "filename" not in dict_udp:
+                    dict_udp["filename"]=[]
+                dict_udp["filename"].append(get_relative_path(str(file)))
             list_of_stats_udp.append(dict_udp)
     create_plots(list_of_stats_tcp,list_of_stats_udp)
 
@@ -48,7 +68,7 @@ def create_plot_for_each_file(list_of_stats,protocol,exclude,destination, figure
             plt.xlabel("start")
             plt.ylabel(str(key))
             plt.legend()
-            plt.savefig(destination+'/plot' +str(i)+".png")
+            plt.savefig(destination+'/plot_' +protocol+"_"+str(key)+".png")
             i+=1
     return i 
         
@@ -62,11 +82,12 @@ def plot_tcp_or_udp(list_of_stats,protocol,exclude,destination,last_figure_numbe
             if str(key) in exclude:
                 continue
             fig = plt.figure(i)
-            plt.plot( round(df['start'],3), round(df[str(key)],3),label=protocol)
+            #print(df["filename"][0])
+            plt.plot( round(df['start'],3), round(df[str(key)],3),label=df["filename"][0])
             plt.xlabel("start")
             plt.ylabel(str(key))
             plt.legend()
-            plt.savefig(destination+'/plot' +str(i)+".png")
+            plt.savefig(destination+'/plot_' +protocol+"_combined_"+str(key)+".png")
             i+=1
         #print(df)
     i= create_plot_for_each_file(list_of_stats,protocol,exclude,destination,i)
